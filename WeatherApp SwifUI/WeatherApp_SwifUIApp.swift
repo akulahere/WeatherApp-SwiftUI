eaf788c4e262259fba70db8889ac43c872951ab4
Dmytro Akulinin <akulinind@gmail.com>
2023-01-23 16:31:26 +0200

    Initial Commit

---
+++ WeatherApp SwifUI/WeatherApp_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwifUIApp.swift
+//  WeatherApp SwifUI
+//
+//  Created by Dmytro Akulinin on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
